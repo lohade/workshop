57714115e6f03b52c760e1bcc08e73647fa92c23
lohade <abhisheklohade8584@gmail.com>
2021-09-11 08:50:27 +0530

    second case

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class TicTacToe {
+
+    static void enterCharacter(){
+        System.out.println("enter X or O to enter of person1:");
+        Scanner sc=new Scanner(System.in);
+        String person1=sc.next();
+        System.out.println("entering computer:");
+        String person2=sc.next();
+
+    }
     public static void main(String[] args){
         char charArray[];
         charArray=new char[10];
+        enterCharacter();
+
+
     }
 
 }
