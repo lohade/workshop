f94f627931d4570fbbd6859683054db1e6ab61c1
lohade <abhisheklohade8584@gmail.com>
2021-09-21 18:46:47 +0530

    showing board

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,30 +1,34 @@
 import java.util.Scanner;
 
 public class TicTacToe {
-
-    static void enterCharacter(){
-        System.out.println("enter X or O to enter of person1:");
-        Scanner sc=new Scanner(System.in);
-        char person1=sc.next().charAt(0);
-
-        char computer;
-        if(person1=='x'){
-            computer='O';
-            System.out.println("computer enter"+ computer);
+    char letter;
+    //public static void userinput(){
+    //    Scanner input = new Scanner(System.in);//
+    public static void inputArray(){
+        String[] board = new String[10];
+        for(int i=1;i<board.length;i++){
+            board[i]=" ";
         }
-        else{
-            computer='x';
-            System.out.println("computer enter"+ computer);
-        }
+    }
+    private static char chooseLetter(Scanner userinput){
+
+        System.out.println("Enter the number you want to choose X or O");
+
+        return userinput.next().charAt(0);
 
     }
-    public static void main(String[] args){
-        char board[];
-        board=new char[10];
-        System.out.println(board);
-        enterCharacter();
-
-
+    // showBoard method is for showing the board
+    public void showBoard(){
+        System.out.println("The board is");
+        System.out.println("| 1 | 2 | 3 |\n| 4 | 5 | 6 | \n| 7 | 8 | 9 |");
     }
 
+    public static void main(String args[]){
+        //   char userinput;
+        TicTacToe obj = new TicTacToe();
+        Scanner input = new Scanner(System.in);
+        char userinput = chooseLetter(input);
+        obj.inputArray();
+        obj.showBoard();
+    }
 }
