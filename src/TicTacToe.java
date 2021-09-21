2665ea5ace65d42b116597f4e8c1df59b6957edc
lohade <88436159+lohade@users.noreply.github.com>
2021-09-21 18:43:23 +0530

    Update TicTacToe.java

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,7 +1,16 @@
 public class TicTacToe {
     public static void main(String[] args){
-        char charArray[];
-        charArray=new char[10];
+        Scanner in = new Scanner(System.in);
+        String[] board = new String[10];
+        String turn = "x";
+        String winner=null;
+        for(int i=1;i< board.length;i++)
+        {
+            board[i]="";
+
+        }
+
     }
 
 }
+
